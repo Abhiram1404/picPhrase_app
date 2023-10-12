dcc8a85491302e8e869328ac7b6f02cadb43df42
Abhiram1404 <abhiramguptak@gmail.com>
2023-10-12 21:10:28 +0530

    Initial commit

---
+++ picPhrase/generateCap/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='generateCap'),
+    path('generate/', views.generate, name='generate'),
+    
+] 
